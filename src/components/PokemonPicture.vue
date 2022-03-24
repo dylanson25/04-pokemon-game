8d83b23c7fa27c0f724c77a37d41946b0416dde1
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2022-03-24 07:47:57 -0600

    end of proyect

--- src/components/PokemonPicture.vue
+++ src/components/PokemonPicture.vue
@@ -2,13 +2,14 @@
   <div class="pokemon-container">
     
     <img 
+      v-if="!showPokemon"
       class="hidden-pokemon"
        :src="imgSrc"
       alt="pokemon"
     />
     
     <img
-      v-if="showPokemon"
+      v-else
       class="fade-in"
       :src="imgSrc"
       alt="pokemon"
