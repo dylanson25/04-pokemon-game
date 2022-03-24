cbebc0f9138de50adb4d7e6719747c22ca10493a
DYLAN VILLARREAL TOSCANO <54459436+dylanson25@users.noreply.github.com>
2022-03-24 06:17:58 -0600

    initial commit

---
+++ src/components/PokemonPicture.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="pokemon-container">
+    
+    <img 
+      class="hidden-pokemon"
+       :src="imgSrc"
+      alt="pokemon"
+    />
+    
+    <img
+      v-if="showPokemon"
+      class="fade-in"
+      :src="imgSrc"
+      alt="pokemon"
+    />
+  </div>
+</template>
+
+<script>
+//require
+export default {
+  props:{
+    PokemonId:{
+      type: Number,
+      require: true,
+      validator(value){
+        return value > 0;
+      }
+    },
+    showPokemon: {
+      type: Boolean,
+      require: true,
+      default: false
+    }
+  },
+  computed:{
+    imgSrc(){
+      return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${this.PokemonId}.svg`
+    }
+  }
+};
+</script>
+
+<style scoped>
+.pokemon-container {
+  height: 200px;
+}
+img {
+  height: 200px;
+  position: absolute;
+  width: 100vw;
+  margin-left: -50vw;
+  user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-user-drag: none;
+  -webkit-user-select: none;
+}
+.hidden-pokemon {
+  filter: brightness(0);
+}
+
+
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <h1>¿Quien es este pokemon?</h1>
+    <PokemonPicture  :PokemonId="4" :showPokemon="true"/>
+    <PokemonOptions />
+</template>
+
+<script>
+
+import PokemonOptions from '@/components/PokemonOptions'
+import PokemonPicture from '@/components/PokemonPicture'
+
+import getPokemonOptions from '@/helpers/getPokemonOptions'
+
+getPokemonOptions()
+export default {
+components: {
+    PokemonOptions,
+    PokemonPicture
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
